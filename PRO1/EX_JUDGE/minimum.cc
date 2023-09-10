f2388dbaaaeb7eaacaca28df5e1ffbbb89284179
xneyk <aguadenadaledgar@gmail.com>
2023-09-11 01:17:24 +0200

    feat: EX3 judge.org

---
+++ PRO1/EX_JUDGE/minimum.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int a, b;
+   cin >> a >> b;
+   // Solució no comprovada a classe pero si acceptada pel judge
+   if(a>=b){
+      cout << b << endl;
+   } else {
+      cout << a << endl;
+   }
+}
