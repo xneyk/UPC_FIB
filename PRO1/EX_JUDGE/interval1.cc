f192465d2b3ac37fdb51a4122b14f182dea5a30e
xneyk <aguadenadaledgar@gmail.com>
2023-09-19 09:27:27 +0200

    feat: Submitted P90226, P51126 and created /IC & /F

---
+++ PRO1/EX_JUDGE/interval1.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int a1, b1, a2, b2;
+   int r1, r2;
+   cin >> a1 >> b1 >> a2 >> b2;
+
+   // Assumim que a1 < b1 i a2 < b2
+
+   if(not((a1 > b2 or b1 < a1) or (a2 > b1 or b2 < a2))) {
+      if(a1>=a2 && a1<=b2){
+         r1 = a1;
+         if(b1 >= a2 and b1 <= b2){
+            r2 = b1;
+         } else {
+            r2 = b2;
+         }
+         cout << '[' <<  r1 << ',' << r2 << ']' << endl;
+      } else {
+         if(a2>=a1 && a2<=b1){
+            r1 = a2;
+            if(b2 >= a1 and b2 <= b1){
+               r2 = b2;
+            } else {
+               r2 = b1;
+            }
+            cout << '[' <<  r1 << ',' << r2 << ']' << endl;
+         }
+      }
+
+   } else {
+      cout << "[]" << endl;
+   }
+}
