1d938acf33c7830c36384b3b2e47e64580793c99
xneyk <aguadenadaledgar@gmail.com>
2023-09-19 09:39:14 +0200

    feat: style correction

--- PRO1/EX_JUDGE/interval1.cc
+++ PRO1/EX_JUDGE/interval1.cc
@@ -9,7 +9,7 @@ int main(){
    // Assumim que a1 < b1 i a2 < b2
 
    if(not((a1 > b2 or b1 < a1) or (a2 > b1 or b2 < a2))) {
-      if(a1>=a2 && a1<=b2){
+      if(a1 >= a2 and a1 <= b2){
          r1 = a1;
          if(b1 >= a2 and b1 <= b2){
             r2 = b1;
@@ -18,7 +18,7 @@ int main(){
          }
          cout << '[' <<  r1 << ',' << r2 << ']' << endl;
       } else {
-         if(a2>=a1 && a2<=b1){
+         if(a2 >= a1 and a2 <= b1){
             r1 = a2;
             if(b2 >= a1 and b2 <= b1){
                r2 = b2;
