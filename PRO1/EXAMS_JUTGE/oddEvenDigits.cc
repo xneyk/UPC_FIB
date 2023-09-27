b26f3c7f7c4411236fbe5eb272f1abd5ade18d58
xneyk <aguadenadaledgar@gmail.com>
2023-09-27 08:24:33 +0200

    fix: document format

--- PRO1/EXAMS_JUTGE/oddEvenDigits.cc
+++ PRO1/EXAMS_JUTGE/oddEvenDigits.cc
@@ -4,41 +4,41 @@ using namespace std;
 int main() {
    int x;
 
-  while (cin >> x) {
-   int y = 0;
-   int z = 0;
-   
-   /**
-    * Amb intencio d'estalbiar memoria, els nombres senars s'asignaran a "z"
-    * i els parells a "y". Posteriorment, el resultat final sera montat a "y"
-    * i "z" es reasignara a "2y".
-   */
-   
-   while (x > 0) {
-      if((x % 10) != 0) {
-         if((x % 10) % 2 != 0 ) {
-            // Es senar
-            y = (y * 10) + (x % 10);
-         } else {
-            // Es parell
-            z = (z * 10) + (x % 10);
+   while (cin >> x) {
+      int y = 0;
+      int z = 0;
+
+      /**
+       * Amb intencio d'estalbiar memoria, els nombres senars s'asignaran a "z"
+       * i els parells a "y". Posteriorment, el resultat final sera montat a "y"
+       * i "z" es reasignara a "2y".
+       */
+
+      while (x > 0) {
+         if ((x % 10) != 0) {
+            if ((x % 10) % 2 != 0) {
+               // Es senar
+               y = (y * 10) + (x % 10);
+            } else {
+               // Es parell
+               z = (z * 10) + (x % 10);
+            }
          }
+         x /= 10;
       }
-      x /= 10;
-   }
 
-   while (y > 0) {
-      x = (10 * x) + (y % 10);
-      y /= 10;
-   }
-   
-   y = x;
+      while (y > 0) {
+         x = (10 * x) + (y % 10);
+         y /= 10;
+      }
 
-   while (z > 0) {
-      y = (10 * y) + (z % 10);
-      z /= 10;
-   }
+      y = x;
 
-   cout << y << ' ' << (2 * y) << endl;
-  }
+      while (z > 0) {
+         y = (10 * y) + (z % 10);
+         z /= 10;
+      }
+
+      cout << y << ' ' << (2 * y) << endl;
+   }
 }
