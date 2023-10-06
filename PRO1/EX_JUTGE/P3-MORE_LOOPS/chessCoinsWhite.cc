4be6e32e5bda09d2fd1bf1a1706e647eacd618f2
xneyk <aguadenadaledgar@gmail.com>
2023-10-06 11:08:44 +0200

    feat: submitted chess problems P42280, P13623

---
+++ PRO1/EX_JUTGE/P3-MORE_LOOPS/chessCoinsWhite.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int r, c;
+   int total_coins = 0;
+   cin >> r >> c;
+
+   for (int i = 0; i < r; ++i) {
+      char coin;
+      for (int j = 0; j < c; ++j) {
+         cin >> coin;
+         if ((j - i) % 2 == 0) total_coins += int(coin) - int('0');
+      }
+   }
+   cout << total_coins << endl;
+}
