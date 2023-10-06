25366e9e2cea1008c676dd7383c6b4cad719b1a0
xneyk <aguadenadaledgar@gmail.com>
2023-10-06 09:27:01 +0200

    feat: submitted P33839

--- PRO1/EX_JUTGE/P3-MORE_LOOPS/sumOfDigits.cc
+++ PRO1/EX_JUTGE/P3-MORE_LOOPS/sumOfDigits.cc
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int n;
    
+   while (cin >> n) {
+      cout << "The sum of the digits of " << n << " is ";
+      
+      int r = 0;
+      while (n > 0) {
+         r += n % 10;
+         n /= 10;
+      }
+      cout << r << '.' << endl;
+   }
 }
