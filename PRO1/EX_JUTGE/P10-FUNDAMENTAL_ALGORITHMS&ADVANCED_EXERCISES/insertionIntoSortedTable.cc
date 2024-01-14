fabab9c1cef59f16c390d76a0ac9f7d0d4a47eaf
xneyk <aguadenadaledgar@gmail.com>
2024-01-14 18:39:40 +0100

    feat: refactor P98179

--- PRO1/EX_JUTGE/P10-FUNDAMENTAL_ALGORITHMS&ADVANCED_EXERCISES/insertionIntoSortedTable.cc
+++ PRO1/EX_JUTGE/P10-FUNDAMENTAL_ALGORITHMS&ADVANCED_EXERCISES/insertionIntoSortedTable.cc
@@ -1,9 +1,14 @@
 //P98179 Insertion into a sorted table
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 void insert(vector<double>& v) {
-   sort(v.begin(), v.end());
+   int i = v.size() - 1;
+   while (v[i] < v[i-1] and i > 0) {
+      double aux = v[i];
+      v[i] = v[i-1];
+      v[i-1] = aux;
+      --i;
+   }
 }
