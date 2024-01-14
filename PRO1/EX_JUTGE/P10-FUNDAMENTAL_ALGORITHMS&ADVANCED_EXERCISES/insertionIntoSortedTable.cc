37de7dbacb43246a9f1cb14fe1cc62d08a0ea32e
xneyk <aguadenadaledgar@gmail.com>
2024-01-14 17:49:30 +0100

    feat: submitted P98179

---
+++ PRO1/EX_JUTGE/P10-FUNDAMENTAL_ALGORITHMS&ADVANCED_EXERCISES/insertionIntoSortedTable.cc
@@ -0,0 +1,9 @@
+//P98179 Insertion into a sorted table
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void insert(vector<double>& v) {
+   sort(v.begin(), v.end());
+}
