7082a0365fe192edceb7f27cc670a206799bac28
xneyk <aguadenadaledgar@gmail.com>
2024-03-21 17:21:49 +0100

    feat:  submitted X80177

--- Q2/PRO2/LAB/S5_ARBRES(1)/REVERSE_TREE/reverseTree.cc
+++ Q2/PRO2/LAB/S5_ARBRES(1)/REVERSE_TREE/reverseTree.cc
@@ -1,1 +1,8 @@
 #include "reverseTree.hh"
+
+// Pre:  
+// Post: retorna el revessat de t.
+BinTree<int> reverseTree(BinTree<int> t) {
+   if (!t.empty()) return BinTree<int>(t.value(), reverseTree(t.right()), reverseTree(t.left()));
+   return t;
+}
