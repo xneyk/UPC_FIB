5aaed0337ad0c47b322ef3f235c8b24b9390131a
xneyk <aguadenadaledgar@gmail.com>
2024-03-25 12:40:07 +0100

    feat: submitted X91713

--- Q2/PRO2/LAB/S6_ARBRES(2)/LONGEST_PATH/longestLeftmostPath.cc
+++ Q2/PRO2/LAB/S6_ARBRES(2)/LONGEST_PATH/longestLeftmostPath.cc
@@ -1,5 +1,16 @@
 #include "longestLeftmostPath.hh"
 
 list<int> longestLeftmostPath(BinTree<int> t) {
-   
+   list<int> left;
+   list<int> right;
+
+   if (!t.empty()) {
+      left = longestLeftmostPath(t.left());
+      left.push_front(t.value());
+      right = longestLeftmostPath(t.right());
+      right.push_front(t.value());
+   }
+
+   if (left.size() >= right.size()) return left;
+   else return right;
 }
