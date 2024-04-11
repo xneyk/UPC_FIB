5e8398e178ad7bd531d46d753d6dfb30f49595e5
xneyk <aguadenadaledgar@gmail.com>
2024-04-11 18:37:11 +0200

    feat: submited round score

--- Q2/PRO2/LAB/S8_MODULARITAT(1)/ROUND_SCORE/roundScore.cc
+++ Q2/PRO2/LAB/S8_MODULARITAT(1)/ROUND_SCORE/roundScore.cc
@@ -1,3 +1,16 @@
 #include "Estudiant.hh"
 #include <vector>
 
+/**
+ * PRE: cert
+ * POST: els estudiants de v amb nota passen a tenir la seva nota arrodonida a la decima mes propera
+*/
+void arrodonir_vector(vector<Estudiant>& v) {
+   int length = v.size();
+   for (int i = 0; i < length; ++i) {
+      if (v[i].te_nota()) {
+         int rounded = 10*(v[i].consultar_nota() + 0.05);
+         v[i].modificar_nota(rounded/10.);
+      }
+   }
+}
