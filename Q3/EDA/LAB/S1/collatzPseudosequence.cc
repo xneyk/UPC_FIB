3714b4f49b092d2cf250cde560061955360e6539
xneyk <aguadenadaledgar@gmail.com>
2024-10-14 11:58:53 +0200

    feat: collazPseudosequence

--- Q3/EDA/LAB/S1/collatzPseudosequence.cc
+++ Q3/EDA/LAB/S1/collatzPseudosequence.cc
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <set>
-#include <queue>
+#include <map>
 
 int next_n(const int& x, const int& y, int n) {
    if (n%2 == 0) return n/2 + x;
@@ -8,35 +7,24 @@ int next_n(const int& x, const int& y, int n) {
 }
 
 int main() {
-   int MAX_ITERATIONS = 100000000;
+   int MAX_OCURRENCE = 100000000;
    int x, y, n;
    while (std::cin >> x >> y >> n) {
       bool cycle_found = false;
-      std::set<int> collection;
-      
-      while (not cycle_found and MAX_ITERATIONS >= 0) {
-         cycle_found = not collection.insert(n).second; // not (true iff insertion took place) ==> if not reapeated
-
-         std::cout << MAX_ITERATIONS << ": " << n << std::endl;
-         // RECORDAR A QUE ITERACION HE METIDO CADA NUMERO, EN EL MOMENTO EN EL QUE TENGA REPETICION SE CUANTO MIDE EL CICLO.
-         if (cycle_found) {
-            std::queue<int> cycle;
-            cycle.push(n);
-
-            while (cycle.front() != next_n(x, y, n)) {
-               n = next_n(x, y, n);
-               cycle.push(n);
-            }
-            std::cout << cycle.size() << std::endl; // we found a cycle so we print its length
-
-            std:: cout << cycle.front() << ' ' << next_n(x, y, n) << std::endl;
+      std::map<int,int> collection;
+      int i = 0;
+      while (not cycle_found and n <= MAX_OCURRENCE) {
+         if (collection.find(n) == collection.end()) {
+            collection[n] = i; // n no habia salido aun
+            n = next_n(x, y, n);
+            ++i;
          }
-         
-
-         n = next_n(x, y, n);
-         --MAX_ITERATIONS;
+         else cycle_found = true;
       }
-
-      if (not cycle_found) std::cout << "last: " << n << std::endl; // we haven't found a cycle so we print the last n.
+      if (cycle_found) {
+         std::cout << i - collection[n] << std::endl;
+      } else {
+         std::cout << n << std::endl;
+      }
    }
 }
