3f010e1ccf6af2a94810a4d47fa6b509c10d0dd7
xneyk <aguadenadaledgar@gmail.com>
2025-03-09 18:26:58 +0100

    fix: document format

--- Q4/AC/Practica03/Practica3MainB.c
+++ Q4/AC/Practica03/Practica3MainB.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
+
 #include "cycle.h"
 #define N 3
 
-int v[10]={1,2,3,4,-10,-11,-12,-13,201,-150};
+int v[10] = {1, 2, 3, 4, -10, -11, -12, -13, 201, -150};
 
 int OperaMat(int Matriz[N][N], int salto);
 
-main(){
-   long int i,a;
+main() {
+   long int i, a;
    int b;
    // Descomentar para medir los ticks
-   //ticks tick1,tick2;
-   
+   // ticks tick1,tick2;
+
    // Descomentar para medir los ticks
-   //tick1=getticks();
-   for (i=0;i<1000;i++)
-     for (a=0;a<1000;a++)
-       b=OperaMat((int (*)[3])v,2);
+   // tick1=getticks();
+   for (i = 0; i < 1000; i++)
+      for (a = 0; a < 1000; a++)
+         b = OperaMat((int(*)[3])v, 2);
    // Descomentar para medir los ticks
-   //tick2=getticks();  
-   //printf("Ciclos = %lf\n",elapsed(tick2,tick1)); 
-       
-   if (b==177)
-     printf("OperaMat BIEN programado.\n");
+   // tick2=getticks();
+   // printf("Ciclos = %lf\n",elapsed(tick2,tick1));
+
+   if (b == 177)
+      printf("OperaMat BIEN programado.\n");
    else
-     printf("OperaMat MAL programado.\n");
+      printf("OperaMat MAL programado.\n");
 }
