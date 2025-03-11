e7bfb60bd9d210d74dc5402f1a4a30fa7e232839
xneyk <aguadenadaledgar@gmail.com>
2025-03-11 08:26:56 +0100

    feat: assembly traduction done

--- Q4/AC/Practica03/Practica3CompletarA.s
+++ Q4/AC/Practica03/Practica3CompletarA.s
@@ -13,12 +13,31 @@ OperaVec:
 	movl	(%eax), %eax
 	movl	%eax, -4(%ebp)
 # Aqui has de introducir el codigo
+# %esi === i
+# %eax === res
+	movl	$1, %esi 					# i = 1
+	movl	-4(%ebp), %eax 			# eax <-- res
+_for:
+	cmpl	12(%ebp), %esi				# i <cmp> elementos
+	jge	_end_for
+_if:
+	movl	8(%ebp), %ecx				# &Vector
+	movl	(%ecx, %esi, 4), %edx	# edx <-- Vector[i]
+	cmpl	-4(%ecx, %esi, 4), %edx	# Vector[i] <cmp> Vector[i-1]
+	jne	_end_if
+	movl	%esi, %eax					# res = i
+_end_if:
+	incl	%esi							# ++i
+	jmp	_for
+_end_for:
+#	movl	%esi, -8(%ebp)				# i de registre a memoria (tot i no ser necesari ja que es destrueix)
+	movl	%eax, -4(%ebp)				# res = res_registre (redundant pero per seguir els criteris de l'estructura)
 
 # El final de la rutina ya esta programado
 	movl	-4(%ebp), %eax
 	popl	%edi
 	popl	%esi
 	popl	%ebx
-	movl %ebp,%esp
-	popl %ebp
+	movl	%ebp,%esp
+	popl	%ebp
 	ret
--- Q4/AC/Practica03/Practica3CompletarB.s
+++ Q4/AC/Practica03/Practica3CompletarB.s
@@ -10,12 +10,42 @@ OperaMat:
 	pushl	%esi
 	pushl	%edi
 # Aqui has de introducir el codigo
+	# %esi === i
+	# %edi === j
+	# %eax === res
 
+	movl	$0, %eax						# res = 0
+	movl	$0, %esi						# i = 0
+_for_i:
+	cmpl	$3, %esi						# i <cmp> 3
+	jge	_end_for_i
+
+	movl	$0, %edi						# j = 0
+_for_j:
+	cmpl	%esi, %edi					# j <cmp> i
+	jg		_end_for_j
+
+	imull	N, %esi, %ecx				# %ecx <- i*N
+	addl	%edi, %ecx					# %ecx <- i*N + j
+	addl	8(%ebp, %ecx, 4), %eax	# res += Matriz[i][j]
+
+	incl	%edi							# ++j
+	jmp	_for_j
+_end_for_j:
+
+	addl	12(%ebp), %esi				# i += salto
+	jmp	_for_i
+_end_for_i:
+
+	movl	%eax, -4(%ebp)				# res -> MEM.
+	movl	%esi, -8(%ebp)				# i_reg -> i_MEM
+	movl	%esi, -12(%ebp)			# j_reg -> j_MEM
+	
 # El final de la rutina ya esta programado
 	movl	-4(%ebp), %eax
 	popl	%edi
 	popl	%esi
-	popl	%ebx	
-	movl %ebp,%esp
-	popl %ebp
+	popl	%ebx
+	movl	%ebp,%esp
+	popl	%ebp
 	ret
