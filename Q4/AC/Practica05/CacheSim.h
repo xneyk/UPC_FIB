deefe939731df96533a95cf154252c6e2aff971d
xneyk <aguadenadaledgar@gmail.com>
2025-03-23 21:08:37 +0100

    feat: AC P05 Cache2AssSim working fine

--- Q4/AC/Practica05/CacheSim.h
+++ Q4/AC/Practica05/CacheSim.h
@@ -12,6 +12,18 @@ void test_and_print (
    unsigned int tag_out
 );
 
+void test_and_print2 (
+   unsigned int address,
+   unsigned int byte,
+   unsigned int bloque_m,
+   unsigned int conj_mc,
+   unsigned int via_mc,
+   unsigned int tag,
+   unsigned int miss,
+   unsigned int replacement,
+   unsigned int tag_out
+);
+
 void init_cache();
 
 void reference(unsigned int address);
--- Q4/AC/Practica05/MiSimulador2.c
+++ Q4/AC/Practica05/MiSimulador2.c
@@ -4,13 +4,24 @@
  * per mantenir la informacio necesaria de la cache
  * */
 
+#define CACHE_SETS 64            // CACHE TOTAL SIZE = 128 linies ==> 64 linies a cada via.
+#define FREE_TAG 0x200000        // last valid tag is 0001 1111 1111 1111 1111 1111 ==> 0x200000 is first invalid tag
+
+unsigned int tag_memory[2][64];  // each tag has 21 bit large
+unsigned char last_way_accessed[64];    // stores which way has been accessed more recently
+
 /* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
- * La cache es inicialitzada al comen�ar cada un dels tests.
+ * La cache es inicialitzada al comencar cada un dels tests.
  * */
 void init_cache() {
    totaltime = 0.0;
    /* Escriu aqui el teu codi */
+   for (int i = 0; i < CACHE_SETS; ++i) {
+      tag_memory[0][i] = FREE_TAG;
+      tag_memory[1][i] = FREE_TAG;
+      last_way_accessed[i] = 1;
+   }
 }
 
 /* La rutina reference es cridada per cada referencia a simular */
@@ -21,21 +32,38 @@ void reference(unsigned int address) {
    unsigned int via_mc;
    unsigned int tag;
    unsigned int miss;         // boolea que ens indica si es miss
-   unsigned int replacement;  // boolea que indica si es reempla�a una linia valida
-   unsigned int tag_out;      // TAG de la linia reempla�ada
+   unsigned int replacement;  // boolea que indica si es reemplaca una linia valida
+   unsigned int tag_out;      // TAG de la linia reemplacada
    float t1, t2;              // Variables per mesurar el temps (NO modificar)
 
    t1 = GetTime();
    /* Escriu aqui el teu codi */
 
+   byte = address & 0x000000000000001f;      // byte number are 5 less significant bits
+   bloque_m = address >> 5;                  // memory_block = address / BLOCK_SIZE
+   conj_mc = bloque_m & 0x000000000000003f;  // 2^6 = way size ==> n_block mod 2^6 are 6 less significant bits of n_block.
+   tag = bloque_m >> 6;                      // tag = n_block / n_sets
+
+   via_mc = 0, miss = false, replacement = false;        // assume hit on way_0
+   if (tag != tag_memory[via_mc][conj_mc]) via_mc = 1;   // assume hit on way_1
+   if (tag != tag_memory[via_mc][conj_mc]) {
+      miss = true;
+      via_mc = !last_way_accessed[conj_mc];  // if last_way == 0 --> 1 ; else 0
+      replacement = miss && tag_memory[via_mc][conj_mc] != FREE_TAG;
+      if (replacement) tag_out = tag_memory[via_mc][conj_mc];
+
+      tag_memory[via_mc][conj_mc] = tag;
+   }
+
+   last_way_accessed[conj_mc] = via_mc;   // update last way accessed in order to make LRU work fine.
+
    /* La funcio test_and_print escriu el resultat de la teva simulacio
     * per pantalla (si s'escau) i comproba si hi ha algun error
-    * per la referencia actual. Tamb� mesurem el temps d'execuci�
+    * per la referencia actual. Tambe mesurem el temps d'execucio
     * */
    t2 = GetTime();
    totaltime += t2 - t1;
-   test_and_print2(address, byte, bloque_m, conj_mc, via_mc, tag,
-                   miss, replacement, tag_out);
+   test_and_print2(address, byte, bloque_m, conj_mc, via_mc, tag, miss, replacement, tag_out);
 }
 
 /* La rutina final es cridada al final de la simulacio */
