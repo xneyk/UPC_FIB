e28367c8a8118bcf9e312e22d26ed8798df60c2e
xneyk <aguadenadaledgar@gmail.com>
2025-06-01 23:48:36 +0200

    feat: bloc 1.3 into done - intro key events

---
+++ Q4/IDI/01_Bloc1_OpenGL/S3/EX1/MyGLWidget.h
@@ -0,0 +1,45 @@
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLWidget>
+#include <QKeyEvent>
+
+#include "glm/glm.hpp"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core {
+   Q_OBJECT
+
+   public:
+      MyGLWidget(QWidget *parent = 0);
+      ~MyGLWidget();
+
+   protected:
+      // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+      virtual void initializeGL();
+
+      // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+      // Tot el que es dibuixa es dibuixa aqui.
+      virtual void paintGL();
+
+      // resize - Es cridat quan canvia la mida del widget
+      virtual void resizeGL(int width, int height);
+
+      // keyPressEvent - Es cridat cada vegada que es prem una tecla
+      virtual void keyPressEvent(QKeyEvent *e);
+
+   private:
+      void creaBuffers();
+      void carregaShaders();
+
+      // attribute locations
+      GLuint vertexLoc;
+      GLuint scaleVarLoc;
+
+      // Program
+      QOpenGLShaderProgram *program;
+
+      float uniform_scale = 0.5f;
+
+      GLuint VAO1;
+      GLint ample, alt;
+};
